2fbc29eb693f5f4abb2238e3d9a0dd3339d4ea91
Shao-An Yin <drrdrem@gmail.com>
2019-04-24 17:06:03 -0700

    Add files via upload

--- SOR.py
+++ SOR.py
@@ -8,9 +8,9 @@ def SOR(A, b, x0, tol, w):
     while iters < max_iter: 
         iters += 1
         for i in range(n): 
-            kp1_sum = np.dot(A[i, 1: (i - 1)], x[1: (i - 1)])     #(20)
+            kp1_sum = np.dot(A[i, :i], x[:i])     #(20)
             k_sum = np.dot(A[i, i+1:], x0[i+1:])
-            x[i] = b[i] - (kp1_sum + k_sum) / A[i, i]               # (22a)
+            x[i] = (b[i] - (kp1_sum + k_sum)) / A[i, i]               # (22a)
             x[i] = np.dot(x[i], w) + np.dot(x0[i], (1 - w))         # (23)
 
         if (np.linalg.norm(x - x0) < tol): break 
